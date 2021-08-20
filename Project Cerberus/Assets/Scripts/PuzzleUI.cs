6aacacf7d8057cf5273ee6c925d9d94e5cd305b0
SAOGonza <samuelortega86@yahoo.com>
2021-08-19 22:08:52 -0700

    Updated the UI
    Lets you know which dog you are controlling. Also changed color and font size to know which dog you are controlling. But the code might need a clean up. Also included two more Text variables for the other two dogs not being controlled.

---
+++ Project Cerberus/Assets/Scripts/PuzzleUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleUI : MonoBehaviour
+{
+    public Text turnCounter;
+    public Text currentDog;
+    public Text secondDog;
+    public Text thirdDog;
+    
+    private GameManager _manager;
+    private int index;
+
+    void Awake()
+    {
+        _manager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Write some code here to update Menu UI - Samuel
+
+        turnCounter.text = $"Turn:\n{_manager.turn}";
+        
+
+
+
+        // Iterate over each move, returning the current and next dog.
+        foreach (Cerberus current in _manager.moveOrder)
+        {
+            // Current dog: Jack
+            Debug.Log("Current dog: " + current.name);
+
+            if (_manager.moveOrder[_manager.currentMove].name == "Jack")
+            {
+                currentDog.text = _manager.moveOrder[0].name;
+                secondDog.text = _manager.moveOrder[1].name;
+                thirdDog.text = _manager.moveOrder[2].name;
+            }
+
+            else if (_manager.moveOrder[_manager.currentMove].name == "Kahuna")
+            {
+                currentDog.text = _manager.moveOrder[1].name;
+                secondDog.text = _manager.moveOrder[2].name;
+                thirdDog.text = _manager.moveOrder[0].name;
+            }
+
+            else if (_manager.moveOrder[_manager.currentMove].name == "Laguna")
+            {
+                currentDog.text = _manager.moveOrder[2].name;
+                secondDog.text = _manager.moveOrder[0].name;
+                thirdDog.text = _manager.moveOrder[1].name;
+            }
+        }
+    }
+}
