fa33ebb8c6d4bc27663b954225bd9f27a7c947d9
gdunford99 <56702202+gdunford99@users.noreply.github.com>
2021-09-09 13:47:08 -0700

    WoodBlock Unodable
    Should allow wood block to be undoable

--- Project Cerberus/Assets/WoodBlock.cs
+++ Project Cerberus/Assets/WoodBlock.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class WoodBlock : BasicBlock
 {
+
+    public class WoodBlockUndoData : UndoData
+    {
+
+        public WoodBlock wood;
+        public bool burned;
+
+        public WoodBlockUndoData(WoodBlock wood, bool burned)
+        {
+            this.wood = wood;
+            this.burned = burned;
+        }
+
+        public override void Load()
+        {
+            
+        }
+
+    }
+
     [SerializeField] private Sprite wholeSprite;
     [SerializeField] private Sprite destroyedSprite;
 
@@ -26,4 +46,23 @@ public class WoodBlock : BasicBlock
         pushableByJacksMultiPush = false;
         pushableByJacksSuperPush = false;
     }
+
+    public void BurntBlock(bool burn)
+    {
+        if (burn == true)
+        {
+            GetComponent<SpriteRenderer>().sprite = destroyedSprite;
+            SetCollisionsEnabled(false);
+            landable = true;
+            pushable = false;
+        }
+
+        if (burn == false)
+        {
+            SetCollisionsEnabled(true);
+            landable = false;
+            pushable = true;
+        }
+    }
+
 }
