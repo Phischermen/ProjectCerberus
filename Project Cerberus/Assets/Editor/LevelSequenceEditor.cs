02de6f78750060a35be3bf2e4fa75e63ef29bf94
Kevin Fischer <46539626+Phischermen@users.noreply.github.com>
2021-09-24 15:52:19 -0700

    Added Level sequence asset and custom project settings

---
+++ Project Cerberus/Assets/Editor/LevelSequenceEditor.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * A custom editor for level sequence.
+ */
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(LevelSequence))]
+    public class LevelSequenceEditor : UnityEditor.Editor
+    {
+        private int _editedWorld;
+        private int _editedLevel;
+        public override void OnInspectorGUI()
+        {
+            var levelSequence = (LevelSequence) target;
+            GUILayout.Label("Level Sequence");
+            // Iterate through every world.
+            for (var i = 0; i < levelSequence.worlds.Count; i++)
+            {
+                var world = levelSequence.worlds[i];
+                // Add foldout group to display world.
+                world.expandedInInspector =
+                    EditorGUILayout.BeginFoldoutHeaderGroup(world.expandedInInspector, $"World{i}");
+                if (world.expandedInInspector)
+                {
+                    // Add button to delete world.
+                    if (GUILayout.Button("Delete World"))
+                    {
+                        // Make this action undoable, and let Zach know he can undo.
+                        UnityEditor.Undo.RecordObject(levelSequence, "Delete World");
+                        NZ.NotifyZach("You deleted their world! Ctrl+Z to undo");
+                        // Remove the world
+                        levelSequence.worlds.Remove(world);
+                    }
+
+                    // Iterate through levels.
+                    for (var j = 0; j < world.levels.Count; j++)
+                    {
+                        var scene = world.levels[j];
+                        // Each level has a row of controls. Start building here.
+                        GUILayout.BeginHorizontal();
+                        world.levels[j] =
+                            (SceneAsset) EditorGUILayout.ObjectField("", scene, typeof(SceneAsset), false);
+                        // Add move up button
+                        if (GUILayout.Button("▲") && j != 0)
+                        {
+                            // Move the scene "up" the list.
+                            world.levels.RemoveAt(j);
+                            world.levels.Insert(j - 1, scene);
+                        }
+
+                        // Add move down button.
+                        if (GUILayout.Button("▼") && j != world.levels.Count)
+                        {
+                            // Move the scene "down" the list.
+                            world.levels.RemoveAt(j);
+                            world.levels.Insert(j + 1, scene);
+                        }
+                        // Add insert button.
+                        if (GUILayout.Button("Ins"))
+                        {
+                            // Display select menu, and insert the scene at the current index.
+                            _editedWorld = i;
+                            _editedLevel = j;
+                            DisplaySelectMenu();
+                        }
+                        // Add delete button.
+                        if (GUILayout.Button("✖"))
+                        {
+                            // Make delete undoable.
+                            UnityEditor.Undo.RecordObject(levelSequence, "Delete Scene");
+                            // Remove the scene
+                            world.levels.RemoveAt(j);
+                        }
+                        // End row of buttons.
+                        GUILayout.EndHorizontal();
+                    }
+                } // End of displaying level controls.
+                // Add add scene button.
+                if (GUILayout.Button("Add Scene"))
+                {
+                    // Display select menu, and append the scene to the bottom of the list.
+                    _editedWorld = i;
+                    _editedLevel = world.levels.Count;
+                    DisplaySelectMenu();
+                }
+
+                EditorGUILayout.EndFoldoutHeaderGroup();
+            }// End of displaying world controls
+            // Add add world button
+            if (GUILayout.Button("Add World"))
+            {
+                // Add the world.
+                levelSequence.AddWorld();
+            }
+
+            if (GUI.changed)
+            {
+                // Apply changes.
+                EditorUtility.SetDirty(levelSequence);
+            }
+        }
+
+        private void DisplaySelectMenu()
+        {
+            // Initialize generic menu.
+            GenericMenu menu = new GenericMenu();
+            // Populate generic menu with scene names from EditorBuildSettings.
+            foreach (var scene in EditorBuildSettings.scenes)
+            {
+                // Get scene path.
+                var name = scene.path;
+                // Extract name from the full path.
+                name = name.Remove(name.LastIndexOf('.'));
+                name = name.Remove(0, name.LastIndexOf('/') + 1);
+                // Add the item to the menu. OnSelectScene is the callback.
+                menu.AddItem(new GUIContent(name), false, OnSelectScene, scene.path);
+            }
+
+            // Show menu.
+            menu.ShowAsContext();
+        }
+
+        private void OnSelectScene(object userdata)
+        {
+            // Cast objects.
+            var levelSequence = (LevelSequence) target;
+            var path = (string) userdata;
+            // Insert the selected scene into the edited field.
+            levelSequence.worlds[_editedWorld].levels
+                .Insert(_editedLevel, AssetDatabase.LoadAssetAtPath<SceneAsset>(path));
+        }
+    }
+}
---
+++ Project Cerberus/Assets/Scripts/LevelSequence.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * This is a custom asset that specifies the order our levels should be played in. It includes a find function that
+ * returns both the level and the world where the scene is located. The active level sequence can be set via the Project
+ * Settings under "Custom Project Settings > Main Sequence".
+ */
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LevelSequence")]
+public class LevelSequence : ScriptableObject
+{
+    [Serializable]
+    public class World
+    {
+        public List<SceneAsset> levels;
+        public bool expandedInInspector;
+
+        public World()
+        {
+            expandedInInspector = true;
+            levels = new List<SceneAsset>();
+        }
+    }
+    
+    public List<World> worlds;
+
+    private void OnEnable()
+    {
+        if (worlds == null)
+        {
+            worlds = new List<World>();
+        }
+    }
+
+    public void AddWorld()
+    {
+        // Initialize world.
+        var world = new World();
+        // Sanity check.
+        if (worlds == null)
+        {
+            worlds = new List<World>();
+        }
+        worlds.Add(world);
+    }
+
+    public void FindCurrentLevelAndWorld(string searchName, out int level, out int world)
+    {
+        // Set to -1 to indicate scene was not found.
+        world = -1;
+        level = -1;
+        // Search through each world's level list for scene via name.
+        for (int i = 0; i < worlds.Count; i++)
+        {
+            world = i;
+            level = worlds[i].levels.FindIndex((asset) => asset.name == searchName);
+            // If level found, break.
+            if (level != -1) break;
+        }
+    }
+
+    public int GetNumberOfWorlds()
+    {
+        return worlds.Count;
+    }
+
+    public int GetNumberOfLevelsInWorld(int world)
+    {
+        // Bound check. Return -1 to indicate out of bounds.
+        if (world < 0 || world > worlds.Count) return -1;
+        return worlds[world].levels.Count;
+    }
+    
+    public SceneAsset GetLevel(int world, int level)
+    {
+        // Bound check. Return null to indicate out of bounds.
+        if (world < 0 || world > worlds.Count) return null;
+        if (level < 0 || level > worlds[world].levels.Count) return null;
+        return worlds[world].levels[level];
+    }
+}
+
