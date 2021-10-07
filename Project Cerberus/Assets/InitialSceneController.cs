bf9c0edb3a11d322ab5ed22551258378fe918555
Kevin Fischer <46539626+Phischermen@users.noreply.github.com>
2021-10-07 11:13:38 -0700

    Added initial scene and controller.

---
+++ Project Cerberus/Assets/InitialSceneController.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * This is the script present in the initial scene loaded when our game starts. This scene will likely be our splash
+ * screen and logos etc.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitialSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene((int) Scenum.Scene.JackTutorial);
+    }
+}
