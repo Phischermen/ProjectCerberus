0c366fad8f9296bffd994ce106e6198e11c120ee
Kevin Fischer <46539626+Phischermen@users.noreply.github.com>
2021-10-26 11:54:18 -0700

    Added buttons to switch and counter editor to make it easier to connect unity events

--- Project Cerberus/Assets/Editor/CounterEditor.cs
+++ Project Cerberus/Assets/Editor/CounterEditor.cs
@@ -1,19 +1,54 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(Counter))]
-public class CounterEditor : UnityEditor.Editor
+namespace Editor
 {
-    // I wish this was actually pickable but Unity makes it too inconvenient to set that up!
-    [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected | GizmoType.Pickable)]
-    static void RenderGizmo(Counter counter, GizmoType gizmoType)
+    [CustomEditor(typeof(Counter))]
+    public class CounterEditor : UnityEditor.Editor
     {
-        var content = new GUIContent($"{counter.name}\nCount: {counter.initialValue}");
-        GUIStyle style = GUI.skin.box;
-        var position = counter.transform.position;
-        Handles.Label(position, content, style);
+        // I wish this was actually pickable but Unity makes it too inconvenient to set that up!
+        [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected | GizmoType.Pickable)]
+        static void RenderGizmo(Counter counter, GizmoType gizmoType)
+        {
+            var content = new GUIContent($"{counter.name}\nCount: {counter.initialValue}");
+            GUIStyle style = GUI.skin.box;
+            var position = counter.transform.position;
+            Handles.Label(position, content, style);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            if (GUILayout.Button("Connect to gate"))
+            {
+                GenericMenu menu = new GenericMenu();
+                foreach (var gate in FindObjectsOfType<Gate>())
+                {
+                    menu.AddItem(new GUIContent(gate.name), false, OnSelectItem, gate);
+                }
+
+                menu.ShowAsContext();
+            }
+        }
+
+        private void OnSelectItem(object userdata)
+        {
+            var counter = (Counter) target;
+            if (userdata is Gate gate)
+            {
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(counter.OnCounterEqualsZero, gate.OpenGate);
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(counter.OnCounterNoLongerEqualsZero,
+                    gate.RequestCloseGate);
+                EditorUtility.SetDirty(target);
+            }
+            else if (userdata is Counter counter1)
+            {
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(counter.OnCounterEqualsZero,
+                    counter1.IncrementCounter);
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(counter.OnCounterNoLongerEqualsZero,
+                    counter1.DecrementCounter);
+                EditorUtility.SetDirty(target);
+            }
+        }
     }
 }
