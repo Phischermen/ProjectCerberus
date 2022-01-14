a273dab1fc941d84caffc74915d1a07b79e6a3bb
Kevin Fischer <46539626+Phischermen@users.noreply.github.com>
2022-01-14 14:58:04 -0800

    Setup endcard failure for Sam.

--- Project Cerberus/Assets/PuzzleUIEndCardFailure.cs
+++ Project Cerberus/Assets/PuzzleUIEndCardFailure.cs
@@ -1,15 +1,38 @@
 ﻿/*
  * PuzzleUIEndCardFailure controls what is displayed on the failure end card, and has a button to reset the level.
  */
+
+using System;
 using UnityEngine;
 
 public class PuzzleUIEndCardFailure : MonoBehaviour
 {
     private GameManager _gameManager;
-
+    // TODO(Sam) Get UI Components.
     private void Awake()
     {
         _gameManager = FindObjectOfType<GameManager>();
+    }
+
+    [Serializable]
+    public struct Endcard
+    {
+        public string title;
+        public Sprite image;
+    }
+
+    [SerializeField] private Endcard[] endcards;
+
+    public enum FailureEndcard
+    {
+        Spiked,
+        FallIntoPit
+    }
+
+    public void DisplayEncard(FailureEndcard endcard)
+    {
+        var endcard1 = endcards[(int) endcard];
+        // TODO(SAM) set UI component fields.
     }
 
     // Button actions
